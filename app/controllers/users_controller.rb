f1bc4e5d57bc6c443d0beb8ee98f03cd601f16d8
kwtuku <ttyttyttitti@gmail.com>
2021-03-14 21:15:23 +0900

    ユーザー情報を更新する機能を追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
+  end
 end
