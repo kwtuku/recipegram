08f4ede9d1ac74de7a8f69ce8f5ef68ce63f976d
kwtuku <ttyttyttitti@gmail.com>
2021-03-17 23:13:45 +0900

    rails g controller recipes index show new editを実行した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
   devise_for :users
   root to: "home#index"
   resources :users
