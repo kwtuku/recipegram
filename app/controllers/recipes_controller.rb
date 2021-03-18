6c38954d13bd17ea8a760931a6eb6ddcdb261b9e
kwtuku <ttyttyttitti@gmail.com>
2021-03-18 19:20:00 +0900

    レシピの新規投稿内容を保存する機能を追加した

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,18 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
+
   def edit
   end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :body, :image)
+    end
 end
