871f8ed3c05d33ee5b7f0cb4a64a94ee57e9c278
kwtuku <ttyttyttitti@gmail.com>
2021-03-20 22:20:04 +0900

    レシピの削除機能を追加した

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,6 +28,12 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path(@recipe)
+  end
+
   private
     def recipe_params
       params.require(:recipe).permit(:title, :body, :image)
