666d708bd89cf702f65b5305cf2f0379e6bed4d8
kwtuku <ttyttyttitti@gmail.com>
2021-03-22 08:56:03 +0900

    新規登録とログイン後のルートを変更した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+    def after_sign_in_path_for(resource)
+      user_path(current_user)
+    end
+
+    def after_sign_up_path_for(resource)
+      user_path(current_user)
+    end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     end
