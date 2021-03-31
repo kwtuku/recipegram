29ec71a4104a8fa83c55dff526d968b9b90da3fe
kwtuku <ttyttyttitti@gmail.com>
2021-04-01 02:58:38 +0900

    フォロー機能を修正した

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,22 +4,22 @@ class RelationshipsController < ApplicationController
   def create
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
+      flash[:notice] = @user.username + 'さんをフォローしました'
+      redirect_back(fallback_location: root_path)
     else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
+      flash.now[:alert] = @user.username + 'さんのフォローに失敗しました'
+      redirect_back(fallback_location: root_path)
     end
   end
 
   def destroy
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
+      flash[:notice] = @user.username + 'さんのフォローを解除しました'
+      redirect_back(fallback_location: root_path)
     else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
+      flash.now[:alert] = @user.username + 'さんのフォロー解除に失敗しました'
+      redirect_back(fallback_location: root_path)
     end
   end
 
