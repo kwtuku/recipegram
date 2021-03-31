17b1f3258a7e32d79f542dc36ebb28eae742feab
kwtuku <ttyttyttitti@gmail.com>
2021-03-31 22:53:39 +0900

    ユーザーをフォローする機能を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,19 @@ class User < ApplicationRecord
   def already_favored?(recipe)
     self.favorites.exists?(recipe_id: recipe.id)
   end
+
+  def follow(other_user)
+    if self != other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
