f236233556537b8fcb0a264880cfd2a151924811
kwtuku <ttyttyttitti@gmail.com>
2021-04-10 02:24:21 +0900

    いいね機能をAJAX化した

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,12 @@
 class FavoritesController < ApplicationController
   def create
+    @recipe = Recipe.find(params[:recipe_id])
     @favorite = current_user.favorites.create(recipe_id: params[:recipe_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
     @favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
