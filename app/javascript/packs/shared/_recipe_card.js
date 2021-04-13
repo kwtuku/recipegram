990e84445cf6e07c0b2df132b8ad424a69baef91
kwtuku <ttyttyttitti@gmail.com>
2021-04-14 01:43:32 +0900

    ドロップダウンメニューが閉じるようにした

--- app/javascript/packs/shared/_recipe_card.js
+++ app/javascript/packs/shared/_recipe_card.js
@@ -10,4 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {
       dropdown.classList.add('is-active');
     });
   });
+
+  window.onclick = function(event) {
+    elements.forEach(function(element) {
+      var button = element.querySelector('button');
+      var dropdown = element.parentNode;
+
+      var menu = document.querySelector('#' + button.getAttribute('aria-controls'));
+
+      if(event.target && element.contains(event.target)) {
+        return;
+      }
+
+      if(event.target && !menu.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+      }
+    });
+  };
 });
