efe94650fa671a51a695fc95a2372e3c08127ef5
kwtuku <ttyttyttitti@gmail.com>
2021-05-09 05:10:57 +0900

    タブをリファクタリングした

--- app/javascript/src/tab.js
+++ app/javascript/src/tab.js
@@ -1,27 +1,23 @@
 export default () => {
   document.addEventListener('DOMContentLoaded', function() {
-    const $tabs = Array.prototype.slice.call(document.querySelectorAll('.tab'), 0);
+    const $tabs = Array.prototype.slice.call(document.querySelectorAll('.tabs li'), 0);
 
     if ($tabs.length > 0) {
-      $tabs.forEach( el => {
-        el.addEventListener('click', () => {
-          $tabs.forEach( el => {
-            el.classList.remove('is-active');
-          });
-          const $tabContents = Array.prototype.slice.call(document.querySelectorAll('.tab-content'), 0);
-          $tabContents.forEach( el => {
-            const $tabContentsChirdlen = Array.prototype.slice.call(el.children, 0);
-            $tabContentsChirdlen.forEach( el => {
-              el.classList.add('display_none');
-            });
+      $tabs.forEach( tab => {
+        tab.addEventListener('click', () => {
+          $tabs.forEach( tab => {
+            tab.classList.remove('is-active');
           });
 
-          el.classList.add('is-active');
-          const $targetTabContentsChirdlen = Array.prototype.slice.call($tabContents[$tabs.indexOf(el)].children, 0);
-          $targetTabContentsChirdlen.forEach( el => {
-            el.classList.remove('display_none');
+          const target = tab.dataset.target;
+          const $target = document.getElementById(target);
+          const $tabContents = Array.prototype.slice.call(document.querySelectorAll('#tab-contents > div'), 0);
+          $tabContents.forEach( tabContent => {
+            tabContent.classList.add('is-hidden');
           });
 
+          tab.classList.add('is-active');
+          $target.classList.remove('is-hidden');
         });
       });
     }
