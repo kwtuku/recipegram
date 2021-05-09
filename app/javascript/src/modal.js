d3c3ff8c731f9e79b54701f55ee3e0fd2b68d362
kwtuku <ttyttyttitti@gmail.com>
2021-05-09 09:23:36 +0900

    モーダルが開いたときにスクロールバー分のマージンを追加、レシピ画像にマウスを合わせると指になるようにした

--- app/javascript/src/modal.js
+++ app/javascript/src/modal.js
@@ -6,21 +6,25 @@ export default () => {
       modals.forEach( modal => {
         const recipeImage = document.querySelector('#recipe-image');
         const html = document.querySelector('html');
+        const body = document.querySelector('body');
         recipeImage.addEventListener('click', () => {
           modal.classList.add('is-active');
           html.classList.add('is-clipped');
+          body.classList.add('mr-4');
         });
 
         const button = document.querySelector('.modal button');
         button.addEventListener('click', () => {
           modal.classList.remove('is-active');
           html.classList.remove('is-clipped');
+          body.classList.remove('mr-4');
         });
 
         const modalBg = document.querySelector('.modal-background');
         modalBg.addEventListener('click', () => {
           modal.classList.remove('is-active');
           html.classList.remove('is-clipped');
+          body.classList.remove('mr-4');
         });
       });
     }
