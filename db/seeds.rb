9ca5840a4a168dece4b7930b952367b36d2db09d
kwtuku <ttyttyttitti@gmail.com>
2021-05-18 18:07:39 +0900

    レシピ、お気に入り、コメントのランダムなサンプルデータを作成できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+Faker::Config.locale = :en
+
 password = "ffffff"
-sample_imgs = %w(
+sample_user_imgs = %w(
   ウキウキな仔犬.jpg
   おはようキバタン.jpg
   ご機嫌のオカメインコ.jpg
@@ -10,20 +12,20 @@ sample_imgs = %w(
 )
 
 User.create!(
-  username:              "山田 太郎",
+  username:              Faker::Lorem.words(number: rand(1..10)).join(" "),
   email:                 "example@example.com",
   password:              password,
   password_confirmation: password,
-  profile:               Faker::Lorem.paragraphs,
-  user_image:            File.open("./public/images/seeds/#{sample_imgs[rand(7)]}"),
+  profile:               Faker::Lorem.paragraph(sentence_count: rand(1..10)),
+  user_image:            File.open("./public/images/seeds/#{sample_user_imgs.sample}"),
 )
 
 user_creation_time = 10
 user_creation_time.times do |n|
-  username  = Faker::Name.unique.name
+  username  = Faker::Lorem.words(number: rand(1..10)).join(" ")
   email = "example#{n+1}@example.com"
-  profile = Faker::Lorem.paragraphs
-  user_image = File.open("./public/images/seeds/#{sample_imgs[rand(7)]}")
+  profile = Faker::Lorem.paragraph(sentence_count: rand(1..10))
+  user_image = File.open("./public/images/seeds/#{sample_user_imgs.sample}")
   User.create!(
     username:              username,
     email:                 email,
@@ -43,5 +45,41 @@ users.each do |user|
     unless user.id == followed.id
       user.relationships.find_or_create_by(follow_id: followed.id)
     end
+  end
+end
+
+sample_recipe_imgs = %w(
+  bread-2178874_1280.webp
+  japanese_dinner_color.png
+  japanese-food-802999_1280.jpg
+  pizza-329523_1280.jpg
+  sashimi-4235910_1280.jpg
+  アサイーボウル.png
+  パンケーキ.png
+  ポークチャップ.png
+  ミールス.png
+)
+total_recipe_creation_time = 0
+users.each do |user|
+  recipe_creation_time = rand(10)
+  recipe_creation_time.times do
+    recipe_title = Faker::Lorem.words(number: rand(1..10)).join(" ")
+    recipe_body = Faker::Lorem.paragraphs(number: rand(5..10)).join("\n")
+    recipe_image = File.open("./public/images/seeds/#{sample_recipe_imgs.sample}")
+    user.recipes.create!(title: recipe_title, body: recipe_body, recipe_image: recipe_image)
+  end
+  total_recipe_creation_time += recipe_creation_time
+end
+
+users.each do |user|
+  rand(total_recipe_creation_time).times do
+    comment_body = Faker::Lorem.paragraphs(number: rand(1..10)).join("\n")
+    user.comments.create!(body: comment_body, recipe_id: rand(total_recipe_creation_time)+1)
+  end
+end
+
+users.each do |user|
+  rand(total_recipe_creation_time).times do
+    user.favorites.find_or_create_by(recipe_id: rand(total_recipe_creation_time)+1)
   end
 end
