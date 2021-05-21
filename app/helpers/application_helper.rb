2014b4ad887cc127af50579e03f5481d59a7035e
kwtuku <ttyttyttitti@gmail.com>
2021-05-22 01:47:05 +0900

    タイトルがページごとに変更されるようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
-  def recipes_show?
-    if controller.controller_name == "recipes" && controller.action_name == "show"
-      true
+  def full_title(page_title = '')
+    base_title = "Recipegram"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
     end
   end
 end
