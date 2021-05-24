7fc7f0654a4950405a6e2d75648344b446371f63
kwtuku <ttyttyttitti@gmail.com>
2021-05-25 02:59:08 +0900

    carrierwaveの画像のサイズと拡張子の制限をした

--- app/uploaders/recipe_image_uploader.rb
+++ app/uploaders/recipe_image_uploader.rb
@@ -35,13 +35,17 @@ class RecipeImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg png webp)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def size_range
+    1..2.megabytes
+  end
 end
