af901789ee16f58013824f2b5836720f3a2bb45f
kwtuku <ttyttyttitti@gmail.com>
2021-06-06 17:52:07 +0900

    プロフィール画像でもプレビューが見られるようにした

--- app/javascript/src/imagePreview.js
+++ app/javascript/src/imagePreview.js
@@ -1,13 +1,14 @@
 export default () => {
   document.addEventListener('turbolinks:load', () => {
-    const recipeImage = document.getElementById('recipe_recipe_image');
+    const image = document.getElementById('recipe_recipe_image') || document.getElementById('user_user_image');
     const cta = document.getElementById('file-cta');
 
-    if (recipeImage) {
-      recipeImage.addEventListener('change', (e) => {
+    if (image) {
+      image.addEventListener('change', (e) => {
         const file = e.target.files[0];
         const blobUrl = window.URL.createObjectURL(file);
         const img = document.getElementById('file-preview');
+        img.classList.remove('is-hidden');
         img.src = blobUrl;
         cta.remove();
       });
