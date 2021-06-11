914334158d9f853761ec7415884950b46e26321a
kwtuku <ttyttyttitti@gmail.com>
2021-06-11 14:20:31 +0900

    検索するカラムを維持できるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,5 +17,17 @@ class HomeController < ApplicationController
               elsif @user_q.conditions.present?
                 @user_q.conditions.first.values.first.value
               end
+
+    @q_column = if request.query_string.include? 'title'
+                  :title_has_every_term
+                elsif request.query_string.include? 'body'
+                  :body_has_every_term
+                elsif request.query_string.include? 'username'
+                  :username_has_every_term
+                elsif request.query_string.include? 'profile'
+                  :profile_has_every_term
+                else
+                  :title_has_every_term
+                end
   end
 end
