b05869626120ab0ab41d926de26de652e937808f
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-14 19:02:17 +0900

    ログインのテストを作成した

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system, js: true do
+RSpec.describe "Users", type: :system do
   describe 'sign up' do
     let(:user) { build :user }
 
-    it 'valid sign up' do
+    it 'valid sign up', js: true do
       visit root_path
 
       expect(page).to have_link '新規登録'
@@ -23,6 +23,31 @@ RSpec.describe "Users", type: :system, js: true do
       expect{ click_button 'signup' }.to change { User.count }.by(1)
 
       expect(page).to have_content 'アカウント登録が完了しました。'
+    end
+  end
+
+  describe 'sign in' do
+    let(:user) { create :user }
+
+    it 'valid sign in', js: true do
+      visit root_path
+
+      within '.container' do
+        expect(page).to have_link 'ログイン'
+
+        click_link 'ログイン'
+      end
+
+      expect(current_path).to eq new_user_session_path
+
+      expect(page).to have_button 'signin', disabled: true
+
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+
+      click_button 'signin'
+
+      expect(page).to have_content 'ログインしました。'
     end
   end
 end
