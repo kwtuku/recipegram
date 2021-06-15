fb10804f343d238394d4a16294db14084300f628
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-15 20:37:02 +0900

    ログインしているときのユーザーのeditアクションの保護テストを追加した

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,8 +8,23 @@ RSpec.describe "Users", type: :request do
     context 'not signed in' do
       it 'redirecto_to new_user_registration_path' do
         get edit_user_path(user)
-
         expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'signed in as wrong user' do
+      it 'redirecto_to user_path(user)' do
+        sign_in other_user
+        get edit_user_path(user)
+        expect(response).to redirect_to user_path(user)
+      end
+    end
+
+    context 'signed in as correct user' do
+      it 'returns a 200 response' do
+        sign_in user
+        get edit_user_path(user)
+        expect(response).to have_http_status(200)
       end
     end
   end
