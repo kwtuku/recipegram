07396f76d50c774fa749621b4f90c533eb95ed2d
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-17 04:34:41 +0900

    ユーザー名とユーザー画像とプロフィールの変更のテストを追加した

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,8 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
-
+    profile { 'This is my profile.' }
+    user_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/user_image_sample.jpg')) }
     trait :with_recipes do
       after(:create) { |user| create_list(:recipe, 5, user: user)}
     end
