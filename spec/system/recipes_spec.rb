1c2a498e78adfe936fe92891e34e618fbc554790
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-17 07:30:19 +0900

    レシピが投稿できるかのテストを作成した

---
+++ spec/system/recipes_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes', type: :system do
+  let(:user) { create :user }
+
+  it 'create recipe', js: true do
+    sign_in user
+    click_link href: new_recipe_path
+    expect(current_path).to eq new_recipe_path
+    expect(page).to have_button 'create_recipe', disabled: true
+    attach_file 'recipe[recipe_image]', "#{Rails.root}/spec/fixtures/recipe_image_sample.jpg", visible: false
+    fill_in 'recipe[title]', with: '野菜炒め'
+    fill_in 'recipe[body]', with: '野菜を切って炒める。'
+    expect{ click_button 'create_recipe' }.to change { Recipe.count }.by(1)
+    expect(page).to have_content 'レシピを投稿しました。'
+  end
+end
