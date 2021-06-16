52ae3ba543394ad3487496da6a5d384f34ca0eb3
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-16 20:03:25 +0900

    アカウント情報を変更することのテストを追加した

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe "Users", type: :system do
     expect(page).to have_content 'ゲストユーザーとしてログインしました。'
   end
 
-  describe 'edit user' do
+  describe 'edit profile' do
     context 'signed in as wrong user' do
       let(:user) { create :user, username: 'user' }
       let(:other_user) { create :user, username: 'other_user' }
@@ -67,6 +67,23 @@ RSpec.describe "Users", type: :system do
 
         expect(page).to have_content '権限がありません。'
       end
+    end
+  end
+
+  describe 'edit user' do
+    let(:user) { create :user, email: 'before@example.com' }
+
+    it 'successful edit', js: true do
+      sign_in user
+      find('.is-hoverable').hover
+      click_link 'アカウント編集'
+      expect(current_path).to eq edit_user_registration_path
+      expect(page).to have_button 'update_account', disabled: true
+      fill_in 'user[email]', with: 'after@example.com'
+      fill_in 'user[current_password]', with: user.password
+      click_button 'update_account'
+      expect(user.reload.email).to eq 'after@example.com'
+      expect(page).to have_content 'アカウント情報を変更しました。'
     end
   end
 end
