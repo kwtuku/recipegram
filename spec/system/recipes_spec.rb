929653f968a85c8806f0f62cda6265274cf41ef2
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-17 08:21:54 +0900

    正しいユーザーではないときはレシピを編集することはできないというテストを作成した

--- spec/system/recipes_spec.rb
+++ spec/system/recipes_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :system do
-  let(:user) { create :user }
+  let(:user) { create :user,  :with_recipes }
+  let(:other_user) { create :user,  :with_recipes }
 
   it 'create recipe', js: true do
     sign_in user
@@ -13,5 +14,16 @@ RSpec.describe 'Recipes', type: :system do
     fill_in 'recipe[body]', with: '野菜を切って炒める。'
     expect{ click_button 'create_recipe' }.to change { Recipe.count }.by(1)
     expect(page).to have_content 'レシピを投稿しました。'
+  end
+
+  describe 'edit recipe' do
+    context 'signed in as wrong user' do
+      it 'can not edit', js: true do
+        sign_in other_user
+        visit edit_recipe_path(user.recipes[0])
+        expect(current_path).to eq recipe_path(user.recipes[0])
+        expect(page).to have_content '権限がありません。'
+      end
+    end
   end
 end
