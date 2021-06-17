47b5a4ff7b715e9f8186e0a891193a38d4f02d9f
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-18 05:21:56 +0900

    recipes#editの保護テストを作成した

---
+++ spec/requests/recipes_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipes', type: :request do
+  describe 'GET /recipes/:id/edit' do
+    let(:user) { create :user, :with_recipes, username: 'user' }
+    let(:other_user) { create :user, username: 'other_user' }
+    let(:user_recipe) { user.recipes[0] }
+
+    context 'not signed in' do
+      it 'redirect_to new_user_session_path' do
+        get edit_recipe_path(user_recipe)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'signed in as wrong user' do
+      it 'redirecto_to recipe_path(user_recipe)' do
+        sign_in other_user
+        get edit_recipe_path(user_recipe)
+        expect(response).to redirect_to recipe_path(user_recipe)
+      end
+    end
+
+    context 'signed in as correct user' do
+      it 'returns a 200 response' do
+        sign_in user
+        get edit_recipe_path(user_recipe)
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
