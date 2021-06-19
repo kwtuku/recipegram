704c8f1304fd6027f5a3ccd876969c1c1cabc518
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-20 01:38:34 +0900

    コメントの通知のテストを作成した

--- spec/system/notifications_spec.rb
+++ spec/system/notifications_spec.rb
@@ -4,7 +4,24 @@ RSpec.describe 'Notifications', type: :system do
   let(:user) { create :user,  :with_recipes }
   let(:other_user) { create :user }
   let(:user_recipe) { user.recipes[0] }
+  let(:other_user_comment) { create :comment, user: other_user, recipe: user_recipe }
 
+  it 'create comment notification', js: true do
+    sign_in user
+    expect(page).to have_no_css '.has-unchecked-notification'
+    other_user_comment.create_comment_notification!(other_user, other_user_comment.id, user_recipe.id)
+    notification = user.passive_notifications[0]
+    expect(notification.checked?).to eq false
+    visit current_path
+    expect(page).to have_css '.has-unchecked-notification'
+    click_link href: notifications_path
+    expect(current_path).to eq notifications_path
+    expect(page).to  have_link "#{notification.visitor.username}"
+    expect(page).to  have_content 'さんが'
+    expect(page).to  have_link "あなたの投稿「#{notification.recipe.title}」"
+    expect(page).to  have_content 'にコメントしました。'
+    expect(notification.reload.checked?).to eq true
+  end
   it 'create favorite notification', js: true do
     sign_in user
     expect(page).to have_no_css '.has-unchecked-notification'
