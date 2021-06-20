ffed0e3c6bc708bda757b5d67c2b1fdcbe901530
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-21 03:53:47 +0900

    レシピの投稿機能のリクエストのテストを作成した

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -14,6 +14,38 @@ RSpec.describe 'Recipes', type: :request do
     end
   end
 
+  describe 'POST /recipes' do
+    let(:user) { create :user }
+    let(:other_user) { create :user }
+
+    context 'not signed in' do
+      it 'redirect_to new_user_session_path' do
+        recipe_params = attributes_for(:recipe)
+        post recipes_path, params: { recipe: recipe_params }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+    context 'signed in as wrong user' do
+      it 'can not create recipe' do
+        sign_in other_user
+        recipe_params = attributes_for(:recipe)
+        expect {
+          post recipes_path, params: { recipe: recipe_params }
+        }.to change { user.recipes.count }.by(0)
+      end
+    end
+    context 'signed in as correct user' do
+      it 'create recipe' do
+        sign_in user
+        recipe_params = attributes_for(:recipe, user: user)
+        expect {
+          post recipes_path, params: { recipe: recipe_params }
+        }.to change { user.recipes.count }.by(1)
+      end
+    end
+  end
+
   describe 'GET /recipes/:id/edit' do
     let(:user) { create :user, :with_recipes, username: 'user' }
     let(:other_user) { create :user, username: 'other_user' }
