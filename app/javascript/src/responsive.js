9e72561b41c51d598ff920b8cfbdd08c8a9f62bb
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-24 10:24:53 +0900

    ウィンドウ幅次第でクラスを削除できるようにした

--- app/javascript/src/responsive.js
+++ app/javascript/src/responsive.js
@@ -1,32 +1,72 @@
 export default () => {
   document.addEventListener('turbolinks:load', () => {
-    const targets = Array.prototype.slice.call(document.querySelectorAll('[data-add-class-mobile]'), 0);
-    if (targets === null) {
+    const addClassTargets = Array.prototype.slice.call(document.querySelectorAll('[data-add-class-mobile]'), 0);
+    const removeClassTargets = Array.prototype.slice.call(document.querySelectorAll('[data-remove-class-mobile]'), 0);
+    if (addClassTargets === null && removeClassTargets === null) {
       return false;
     }
 
-    targets.forEach( target => {
-      const className = target.dataset.addClassMobile
+    const addClass = () => {
+      addClassTargets.forEach( target => {
+        const className = target.dataset.addClassMobile
 
-      window.addEventListener('resize', () => {
-        const windowWidth = window.innerWidth;
+        window.addEventListener('resize', () => {
+          const windowWidth = window.innerWidth;
 
-        if (windowWidth <= 768) {
-          target.classList.add(className);
-        } else {
-          target.classList.remove(className);
-        }
+          if (windowWidth <= 768) {
+            target.classList.add(className);
+          } else {
+            target.classList.remove(className);
+          }
+        });
+
+        window.addEventListener('pageshow', () => {
+          const windowWidth = window.innerWidth;
+
+          if (windowWidth <= 768) {
+            target.classList.add(className);
+          } else {
+            target.classList.remove(className);
+          }
+        });
       });
+    };
 
-      window.addEventListener('pageshow', () => {
-        const windowWidth = window.innerWidth;
+    const removeClass = () => {
+      removeClassTargets.forEach( target => {
+        const className = target.dataset.removeClassMobile
 
-        if (windowWidth <= 768) {
-          target.classList.add(className);
-        } else {
-          target.classList.remove(className);
-        }
+        window.addEventListener('resize', () => {
+          const windowWidth = window.innerWidth;
+
+          if (windowWidth <= 768) {
+            target.classList.remove(className);
+          } else {
+            target.classList.add(className);
+          }
+        });
+
+        window.addEventListener('pageshow', () => {
+          const windowWidth = window.innerWidth;
+
+          if (windowWidth <= 768) {
+            target.classList.remove(className);
+          } else {
+            target.classList.add(className);
+          }
+        });
       });
-    });
+    };
+
+    if (addClassTargets !== null && removeClassTargets === null) {
+      addClass();
+    }
+    if (addClassTargets === null && removeClassTargets !== null) {
+      removeClass();
+    }
+    if (addClassTargets !== null && removeClassTargets !== null) {
+      addClass();
+      removeClass();
+    }
   });
 }
