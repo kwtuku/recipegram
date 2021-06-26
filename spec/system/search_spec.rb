6ced1a9f222bde854891e69d9f7aa52e60137389
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-06-27 06:33:49 +0900

    各条件の検索結果数が正しいかを確かめるテストに変更した

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Search', type: :system do
-  let!(:kotteri_miso_ramen) { create :recipe, title: 'こってり味噌ラーメン' }
-  let!(:kotteri_shio_ramen) { create :recipe, title: 'こってり塩ラーメン' }
-  let!(:kotteri_shoyu_ramen) { create :recipe, title: 'こってりしょうゆラーメン' }
-  let!(:kotteri_tonkotsu_ramen) { create :recipe, title: 'こってり豚骨ラーメン' }
-  let!(:sappari_miso_ramen) { create :recipe, title: 'さっぱり味噌ラーメン' }
-  let!(:sappari_shio_ramen) { create :recipe, title: 'さっぱり塩ラーメン' }
-  let!(:sappari_shoyu_ramen) { create :recipe, title: 'さっぱりしょうゆラーメン' }
-  let!(:sappari_tonkotsu_ramen) { create :recipe, title: 'さっぱり豚骨ラーメン' }
+  let!(:miso_ramen) { create :recipe, title: '味噌ラーメンの作り方', body: '味噌ラーメンの作り方です。' }
+  let!(:miso_soup) { create :recipe, title: '味噌汁', body: '味噌汁の作り方です。' }
+  let!(:miso_katsudon) { create :recipe, title: '味噌カツ丼', body: '味噌カツ丼の作り方です。' }
+  let!(:miso_udon) { create :recipe, title: '味噌煮込みうどん', body: '味噌煮込みうどんの作り方です。' }
+  let!(:miso) { create :recipe, title: '味噌', body: 'みその作り方です。' }
+  let!(:tonkotsu_ramen) { create :recipe, title: '豚骨ラーメン', body: '豚骨ラーメンの作り方です。' }
+
+  let!(:miso_ramen_man) { create :user, username: '味噌ラーメン大好きマン', profile: '味噌ラーメン大好き' }
+  let!(:alice) { create :user, username: 'アリス', profile: '味噌ラーメンの作り方知ってます。' }
+  let!(:bob) { create :user, username: 'ボブ', profile: '味噌カツ丼の作り方知りたい！' }
+  let!(:carol) { create :user, username: '味噌好きキャロル', profile: 'みそ汁が作れます。' }
+  let!(:dave) { create :user, username: 'dave', profile: 'I am dave.' }
 
   it 'returns no result', js: true do
     visit root_path
@@ -18,39 +22,44 @@ RSpec.describe 'Search', type: :system do
     click_button 'search'
     expect(page).to have_content '「ごはん」は見つかりませんでした。'
     expect(page).to have_selector '.rspec_recipe_title_results_size', text: '0'
+    expect(page).to have_selector '.rspec_recipe_body_results_size', text: '0'
+    expect(page).to have_selector '.rspec_user_username_results_size', text: '0'
+    expect(page).to have_selector '.rspec_user_profile_results_size', text: '0'
   end
-  it 'returns result from recipe titles', js: true do
+
+  it 'returns correct results size', js: true do
     visit root_path
     click_link href: search_path
     expect(page).to have_button 'search', disabled: true
     fill_in 'search_query', with: '味噌'
     click_button 'search'
-    expect(page).to have_link 'こってり味噌ラーメン'
-    expect(page).to have_link 'さっぱり味噌ラーメン'
-    expect(page).to have_selector '.rspec_recipe_title_results_size', text: '2'
+    expect(page).to have_selector '.rspec_recipe_title_results_size', text: '5'
+    expect(page).to have_selector '.rspec_recipe_body_results_size', text: '4'
+    expect(page).to have_selector '.rspec_user_username_results_size', text: '2'
+    expect(page).to have_selector '.rspec_user_profile_results_size', text: '3'
   end
-  it 'and search using space', js: true do
+
+  it 'can AND search using space', js: true do
     visit root_path
     click_link href: search_path
     expect(page).to have_button 'search', disabled: true
-    fill_in 'search_query', with: 'こってり ラーメン'
+    fill_in 'search_query', with: '味噌 作り方'
     click_button 'search'
-    expect(page).to have_link 'こってり味噌ラーメン'
-    expect(page).to have_link 'こってり塩ラーメン'
-    expect(page).to have_link 'こってりしょうゆラーメン'
-    expect(page).to have_link 'こってり豚骨ラーメン'
-    expect(page).to have_selector '.rspec_recipe_title_results_size', text: '4'
+    expect(page).to have_selector '.rspec_recipe_title_results_size', text: '1'
+    expect(page).to have_selector '.rspec_recipe_body_results_size', text: '4'
+    expect(page).to have_selector '.rspec_user_username_results_size', text: '0'
+    expect(page).to have_selector '.rspec_user_profile_results_size', text: '2'
   end
+
   it 'and search using zenkaku spece', js: true do
     visit root_path
     click_link href: search_path
     expect(page).to have_button 'search', disabled: true
-    fill_in 'search_query', with: 'こってり　ラーメン'
+    fill_in 'search_query', with: '味噌　作り方'
     click_button 'search'
-    expect(page).to have_link 'こってり味噌ラーメン'
-    expect(page).to have_link 'こってり塩ラーメン'
-    expect(page).to have_link 'こってりしょうゆラーメン'
-    expect(page).to have_link 'こってり豚骨ラーメン'
-    expect(page).to have_selector '.rspec_recipe_title_results_size', text: '4'
+    expect(page).to have_selector '.rspec_recipe_title_results_size', text: '1'
+    expect(page).to have_selector '.rspec_recipe_body_results_size', text: '4'
+    expect(page).to have_selector '.rspec_user_username_results_size', text: '0'
+    expect(page).to have_selector '.rspec_user_profile_results_size', text: '2'
   end
 end
