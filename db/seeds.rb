dcc85a9ffb5872eee082941c1732166dac88413b
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-01 09:17:52 +0900

    デモデータの作り方を更新した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,55 @@
 Faker::Config.locale = :en
 
-def create_user(user_creation_time)
-  password = "ffffff"
-  sample_user_imgs = %w(
-    ウキウキな仔犬.jpg
-    おはようキバタン.jpg
-    ご機嫌のオカメインコ.jpg
-    ニヒルなキバタン.jpg
-    呼ぶネコ.jpg
-    初めましてイワシャコです.jpg
-    分かんなくなった柴犬.jpg
-  )
+def create_characteristic_user_recipe_comment
+  common_password = 'fffffr'
 
   User.create!(
-    username:              Faker::Lorem.words(number: rand(1..10)).join(" "),
-    email:                 "example@example.com",
-    password:              password,
-    password_confirmation: password,
-    profile:               Faker::Lorem.paragraph(sentence_count: rand(1..20)),
-    user_image:            File.open("./public/images/seeds/#{sample_user_imgs.sample}"),
+    username:              "very_l#{'o'*100}ng_word",
+    email:                 'long@example.com',
+    password:              common_password,
+    password_confirmation: common_password,
+    profile:               "very_l#{'o'*500}ng_word",
+    user_image:            File.open("./db/fixtures/user/user_sample_#{rand(1..15)}.jpg"),
   )
 
+  user = User.find_by(email: 'long@example.com')
+  user.recipes.create!(
+    title:        "very_l#{'o'*100}ng_word",
+    body:         "very_l#{'o'*900}ng_word",
+    recipe_image: File.open("./db/fixtures/recipe/recipe_sample_#{rand(1..15)}.jpg")
+  )
+
+  recipe = Recipe.last
+  user.comments.create!(
+    recipe_id: recipe.id,
+    body:      "very_l#{'o'*500}ng_word",
+  )
+end
+
+def create_user(user_creation_time)
+  common_password = 'fffffr'
+
   user_creation_time.times do |n|
-    username  = Faker::Lorem.words(number: rand(1..10)).join(" ")
+    username  = Faker::Lorem.words(number: rand(1..10)).join(' ')
     email = "example#{n+1}@example.com"
-    profile = Faker::Lorem.paragraph(sentence_count: rand(1..20))
-    user_image = File.open("./public/images/seeds/#{sample_user_imgs.sample}")
+    profile = <<~TEXT
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(3..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..8)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(3..8)).join(' ')}
+    TEXT
+    user_image = File.open("./db/fixtures/user/user_sample_#{rand(1..15)}.jpg")
+
     User.create!(
       username:              username,
       email:                 email,
-      password:              password,
-      password_confirmation: password,
+      password:              common_password,
+      password_confirmation: common_password,
       profile:               profile,
       user_image:            user_image,
     )
@@ -51,29 +70,34 @@ def create_relationship
 end
 
 def create_recipe(recipe_creation_time)
-  sample_recipe_imgs = %w(
-    bread-2178874_1280.webp
-    japanese_dinner_color.png
-    japanese-food-802999_1280.jpg
-    pizza-329523_1280.jpg
-    sashimi-4235910_1280.jpg
-    アサイーボウル.png
-    パンケーキ.png
-    ポークチャップ.png
-    ミールス.png
-  )
-
   recipe_creation_time.times do
-    recipe_title = Faker::Lorem.words(number: rand(1..10)).join(" ")
-    recipe_body = Faker::Lorem.paragraphs(number: rand(5..40)).join("\n")
-    recipe_image = File.open("./public/images/seeds/#{sample_recipe_imgs.sample}")
+    recipe_title = Faker::Lorem.words(number: rand(1..10)).join(' ')
+    recipe_body = <<~TEXT
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(3..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..8)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(3..8)).join(' ')}
+    TEXT
+    recipe_image = File.open("./db/fixtures/recipe/recipe_sample_#{rand(1..15)}.jpg")
+
     User.all.sample.recipes.create!(title: recipe_title, body: recipe_body, recipe_image: recipe_image)
   end
 end
 
 def create_comment(comment_creation_time)
   comment_creation_time.times do
-    comment_body = Faker::Lorem.paragraphs(number: rand(1..10)).join("\n")
+    comment_body = <<~TEXT
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..8)).join(' ')}
+    TEXT
+
     User.all.sample.comments.create!(body: comment_body, recipe_id: Recipe.all.sample.id)
   end
 end
@@ -86,13 +110,24 @@ end
 
 def update_recipe(recipe_updating_time)
   recipe_updating_time.times do
-    recipe_title = Faker::Lorem.words(number: rand(1..10)).join(" ")
-    recipe_body = Faker::Lorem.paragraphs(number: rand(5..40)).join("\n")
+    recipe_title = Faker::Lorem.words(number: rand(1..10)).join(' ')
+    recipe_body = <<~TEXT
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(3..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..8)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..5)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(1..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(5..10)).join(' ')}
+      #{Faker::Lorem.paragraphs(number: rand(3..8)).join(' ')}
+    TEXT
+
     Recipe.all.sample.update!(title: recipe_title, body: recipe_body)
   end
 end
 
-def create_notification(notification_creation_time, user_id)
+def create_notification_for_one_user(notification_creation_time, user_id)
   user = User.find(user_id)
   other_user_ids = User.ids - [user.id]
 
@@ -109,10 +144,11 @@ def create_notification(notification_creation_time, user_id)
   end
 end
 
-create_user(100)
+create_characteristic_user_recipe_comment
+create_user(time)
 create_relationship
-create_recipe(100)
-create_comment(500)
-create_favorite(500)
-update_recipe(10)
-create_notification(10, 52)
+create_recipe(time)
+create_comment(time)
+create_favorite(time)
+update_recipe(time)
+create_notification_for_one_user(time, user_id)
