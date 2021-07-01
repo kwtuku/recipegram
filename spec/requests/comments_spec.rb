54728bf5075428791abe30fd109b0152bf0e2adb
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-01 14:40:10 +0900

    comments#createのリクエストスペックを作成した

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
+  describe 'POST /recipes/:recipe_id/comments' do
+    let(:alice) { create :user }
+    let(:alice_recipe) { create :recipe, user: alice }
+
+    context 'not signed in' do
+      it 'redirect_to new_user_session_path' do
+        comment_params = attributes_for(:comment)
+        expect {
+          post recipe_comments_path(alice_recipe), params: { comment: comment_params }
+        }.to change { Comment.count }.by(0)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'signed in' do
+      it 'create comment' do
+        sign_in alice
+        comment_params = attributes_for(:comment)
+        expect {
+          post recipe_comments_path(alice_recipe), params: { comment: comment_params }
+        }.to change { alice.comments.count }.by(1)
+      end
+    end
+  end
+
   describe 'DELETE /recipes/:recipe_id/comments/:id' do
     let(:alice) { create :user }
     let(:bob) { create :user }
