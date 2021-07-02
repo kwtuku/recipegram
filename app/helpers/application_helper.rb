34862a3b2e20f495cbe7625802fb89470fefbe26
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-02 22:04:36 +0900

    フォローしている人かフォロワー一覧もしくはフォローしている人かフォロワーがいないという説明を表示するヘルパーを作成した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,24 @@ module ApplicationHelper
       "#{page_title} - #{base_title}"
     end
   end
+
+  def follows_or_no_follow_description(follows, user)
+    if follows.exists?
+      render partial: 'users/user_list', collection: follows, as: 'user'
+    else
+      no_follow_description(user)
+    end
+  end
+
+  def no_follow_description(user)
+    if user == current_user && controller.action_name == 'followings'
+      tag.p 'まだ誰もフォローしていません'
+    elsif user != current_user && controller.action_name == 'followings'
+      tag.p "#{user.username}さんはまだ誰もフォローしていません"
+    elsif user == current_user && controller.action_name == 'followers'
+      tag.p 'まだフォロワーがいません'
+    else
+      tag.p "#{user.username}さんはまだ誰もフォローしていません"
+    end
+  end
 end
