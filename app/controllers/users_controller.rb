bb80372263d839b975c008f4d3278e2831e53a7a
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-04 23:04:58 +0900

    following?メソッドによるn+1を解消した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,12 +41,12 @@ class UsersController < ApplicationController
 
   def followings
     @user = User.find(params[:user_id])
-    @follows = @user.followings.limit(40)
+    @follows = @user.followings.preload(:followings).limit(40)
   end
 
   def followers
     @user = User.find(params[:user_id])
-    @follows = @user.followers.limit(40)
+    @follows = @user.followers.preload(:followings).limit(40)
     render 'followings'
   end
 
