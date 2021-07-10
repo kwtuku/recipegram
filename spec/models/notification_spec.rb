a366614538e1ee1a872fb4033453ba85cda22dca
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-11 03:33:21 +0900

    コメント通知を送るユーザーのidが正しいことを確かめるモデルスペックを追加した

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -52,6 +52,42 @@ RSpec.describe Notification, type: :model do
     end
   end
 
+  describe 'self.comment_notification_receiver_ids(comment)' do
+    let(:alice_recipe) { create :recipe, user: alice }
+    let!(:bob_comment) { create :comment, user: bob, recipe: alice_recipe }
+    let(:carol) { create :user }
+    let!(:carol_comment) { create :comment, user: carol, recipe: alice_recipe }
+
+    context 'comment user != recipe user' do
+      let(:dave) { create :user }
+      let(:dave_comment) { create :comment, user: dave, recipe: alice_recipe }
+
+      it 'has recipe user id' do
+        expect(Notification.comment_notification_receiver_ids(dave_comment)).to include alice.id
+      end
+
+      it 'has other comment user id' do
+        expect(Notification.comment_notification_receiver_ids(dave_comment)).to include bob.id, carol.id
+      end
+
+      it 'does not have comment user id' do
+        expect(Notification.comment_notification_receiver_ids(dave_comment)).to_not include dave.id
+      end
+    end
+
+    context 'comment user == recipe user' do
+      let(:alice_comment) { create :comment, user: alice, recipe: alice_recipe }
+
+      it 'does not have recipe user id' do
+        expect(Notification.comment_notification_receiver_ids(alice_comment)).to_not include alice.id
+      end
+
+      it 'has other comment user id' do
+        expect(Notification.comment_notification_receiver_ids(alice_comment)).to include bob.id, carol.id
+      end
+    end
+  end
+
   describe 'self.create_favorite_notification(favorite)' do
     let(:alice_recipe) { create :recipe, user: alice }
 
