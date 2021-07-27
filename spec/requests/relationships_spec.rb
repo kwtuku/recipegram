899298e4539040835bfdcfffb90ceb66690b38ab
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-27 19:01:01 +0900

    relationshipのリクエストスペックでログインしていない場合のときに確かめるステータスコードを修正した

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -6,47 +6,46 @@ RSpec.describe 'Relationships', type: :request do
     let(:bob) { create :user, :no_image }
 
     context 'when not signed in' do
-      it 'does not create relationship' do
+      it 'does not increase Relationship count' do
         expect {
           post relationships_path, params: { follow_id: bob.id }, xhr: true
         }.to change { Relationship.count }.by(0)
       end
 
-      it 'does not create follower' do
+      it 'does not increase User.followers count' do
         expect {
           post relationships_path, params: { follow_id: bob.id }, xhr: true
         }.to change { bob.followers.count }.by(0)
       end
 
-      it 'does not create following' do
+      it 'does not increase User.followings count' do
         expect {
           post relationships_path, params: { follow_id: bob.id }, xhr: true
         }.to change { alice.followings.count }.by(0)
       end
 
-      it 'returns a 200 response' do
-        sign_in alice
+      it 'returns a 401 response' do
         post relationships_path, params: { follow_id: bob.id }, xhr: true
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(401)
       end
     end
 
     context 'when signed in' do
-      it 'creates relationship' do
+      it 'increases Relationship count' do
         sign_in alice
         expect {
           post relationships_path, params: { follow_id: bob.id }, xhr: true
         }.to change { Relationship.count }.by(1)
       end
 
-      it 'creates follower' do
+      it 'increases User.followers count' do
         sign_in alice
         expect {
           post relationships_path, params: { follow_id: bob.id }, xhr: true
         }.to change { bob.followers.count }.by(1)
       end
 
-      it 'creates following' do
+      it 'increases User.followings count' do
         sign_in alice
         expect {
           post relationships_path, params: { follow_id: bob.id }, xhr: true
@@ -69,47 +68,46 @@ RSpec.describe 'Relationships', type: :request do
     end
 
     context 'when not signed in' do
-      it 'does not destroy relationship' do
+      it 'does not decrease Relationship count' do
         expect {
           delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
         }.to change { Relationship.count }.by(0)
       end
 
-      it 'does not destroy follower' do
+      it 'does not decrease User.followers count' do
         expect {
           delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
         }.to change { bob.followers.count }.by(0)
       end
 
-      it 'does not destroy following' do
+      it 'does not decrease User.followings count' do
         expect {
           delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
         }.to change { alice.followings.count }.by(0)
       end
 
-      it 'returns a 200 response' do
-        sign_in alice
+      it 'returns a 401 response' do
         delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(401)
       end
     end
 
     context 'when signed in' do
-      it 'destroys relationship' do
+      it 'decreases Relationship count' do
         sign_in alice
         expect {
           delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
         }.to change { Relationship.count }.by(-1)
       end
 
-      it 'destroys follower' do
+      it 'decreases User.followers count' do
         sign_in alice
         expect {
           delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
         }.to change { bob.followers.count }.by(-1)
       end
 
-      it 'destroys following' do
+      it 'decreases User.followings count' do
         sign_in alice
         expect {
           delete relationship_path(@relationship), params: { follow_id: bob.id }, xhr: true
