22d26a2e28e614dedabb14fe641b7b86d4d0185b
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-07-28 16:13:06 +0900

    パスワードのマスクにtitle属性を加えた

--- app/javascript/src/removePasswordMask.js
+++ app/javascript/src/removePasswordMask.js
@@ -8,14 +8,17 @@ export default () => {
     passwordMasks.forEach( passwordMask => {
       const input = passwordMask.previousElementSibling.firstElementChild;
       const icon = passwordMask.querySelector('i');
+      const button = passwordMask.querySelector('button');
 
       passwordMask.addEventListener('click', () => {
         if (input.getAttribute('type') === 'password') {
           input.setAttribute('type', 'text');
           icon.setAttribute('class','far fa-eye');
+          button.setAttribute('title','パスワードを非表示にする');
         } else {
           input.setAttribute('type', 'password');
           icon.setAttribute('class','far fa-eye-slash');
+          button.setAttribute('title','パスワードを表示する');
         }
       });
 
@@ -23,12 +26,14 @@ export default () => {
         if(!e.target.closest('.password-area')) {
           input.setAttribute('type', 'password');
           icon.setAttribute('class','far fa-eye-slash');
+          button.setAttribute('title','パスワードを非表示にする');
         }
       });
 
       window.addEventListener('scroll', () => {
         input.setAttribute('type', 'password');
         icon.setAttribute('class','far fa-eye-slash');
+        button.setAttribute('title','パスワードを表示する');
       });
     });
   });
