01eca52483ddaddcca4d31ef87b581de9d776679
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-09 08:36:45 +0900

    プロフィール編集ページでも文字数がカウントされるようにした

--- app/javascript/src/countCharacters.js
+++ app/javascript/src/countCharacters.js
@@ -3,13 +3,41 @@ export default () => {
     const inputs = Array.prototype.slice.call(document.querySelectorAll('input'), 0);
     const textarea = document.querySelector('textarea');
 
+    const nickname = document.getElementById('user_nickname');
+    const profile = document.getElementById('user_profile');
     const recipeTitle = document.getElementById('recipe_title');
     const recipeBody = document.getElementById('recipe_body');
     const commentBody = document.getElementById('comment_body');
 
+    const updateUser = document.getElementById('update_user');
     const createRecipe = document.getElementById('create_recipe');
     const updateRecipe = document.getElementById('update_recipe');
     const createComment = document.getElementById('create_comment');
+
+    if (updateUser) {
+      const countNicknameCharacters = document.getElementById('count-nickname-characters');
+      nickname.addEventListener('input', () => {
+        countNicknameCharacters.textContent = nickname.value.length;
+        if (nickname.value.length > 30) {
+          nickname.classList.add('is-danger')
+          countNicknameCharacters.classList.add('has-text-danger')
+        } else {
+          nickname.classList.remove('is-danger')
+          countNicknameCharacters.classList.remove('has-text-danger')
+        }
+      });
+      const countProfileCharacters = document.getElementById('count-profile-characters');
+      profile.addEventListener('input', () => {
+        countProfileCharacters.textContent = profile.value.length;
+        if (profile.value.length > 500) {
+          profile.classList.add('is-danger')
+          countProfileCharacters.classList.add('has-text-danger')
+        } else {
+          profile.classList.remove('is-danger')
+          countProfileCharacters.classList.remove('has-text-danger')
+        }
+      });
+    }
 
     if (createRecipe || updateRecipe) {
       const countRecipeTitleCharacters = document.getElementById('count-recipe-title-characters');
