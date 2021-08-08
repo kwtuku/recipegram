3b240d1fa9c281768569400c524b0316f86abcd8
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-09 08:03:31 +0900

    コメント作成時に文字数がカウントされるようにした

--- app/javascript/src/countCharacters.js
+++ app/javascript/src/countCharacters.js
@@ -5,9 +5,11 @@ export default () => {
 
     const recipeTitle = document.getElementById('recipe_title');
     const recipeBody = document.getElementById('recipe_body');
+    const commentBody = document.getElementById('comment_body');
 
     const createRecipe = document.getElementById('create_recipe');
     const updateRecipe = document.getElementById('update_recipe');
+    const createComment = document.getElementById('create_comment');
 
     if (createRecipe || updateRecipe) {
       const countRecipeTitleCharacters = document.getElementById('count-recipe-title-characters');
@@ -32,6 +34,20 @@ export default () => {
         } else {
           recipeBody.classList.remove('is-danger')
           countRecipeBodyCharacters.classList.remove('has-text-danger')
+        }
+      });
+    }
+
+    if (createComment) {
+      const countCommentBodyCharacters = document.getElementById('count-comment-body-characters');
+      textarea.addEventListener('input', () => {
+        countCommentBodyCharacters.textContent = commentBody.value.length;
+        if (commentBody.value.length > 500) {
+          commentBody.classList.add('is-danger')
+          countCommentBodyCharacters.classList.add('has-text-danger')
+        } else {
+          commentBody.classList.remove('is-danger')
+          countCommentBodyCharacters.classList.remove('has-text-danger')
         }
       });
     }
