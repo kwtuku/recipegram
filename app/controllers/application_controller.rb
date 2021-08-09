9a493c4a086fc42687d0b97b1f136bad8422559d
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-09 10:54:05 +0900

    プロフィール編集ページではなくアカウント編集ページからusernameの変更を行えるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :nickname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
 end
