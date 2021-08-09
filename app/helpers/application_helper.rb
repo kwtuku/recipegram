149583d075772b995f5d2f31f43d8e80f3e4a71c
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-09 18:34:26 +0900

    デフォルトという検索結果のソートを削除した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,38 +54,38 @@ module ApplicationHelper
   end
 
   def selected_recipe_sort_order
-    if request.query_parameters[:sort] == 'updated_at' && request.query_parameters[:order] == 'asc'
+    if request.query_parameters[:sort] == 'updated_at' && request.query_parameters[:order] == 'desc'
+    '更新日が新しい順'
+    elsif request.query_parameters[:sort] == 'updated_at' && request.query_parameters[:order] == 'asc'
       '更新日が古い順'
-    elsif request.query_parameters[:sort] == 'updated_at' && request.query_parameters[:order] == 'desc'
-      '更新日が新しい順'
-    elsif request.query_parameters[:sort] == 'favorites_count' && request.query_parameters[:order] == 'asc'
-      'いいねが少ない順'
     elsif request.query_parameters[:sort] == 'favorites_count' && request.query_parameters[:order] == 'desc'
       'いいねが多い順'
-    elsif request.query_parameters[:sort] == 'comments_count' && request.query_parameters[:order] == 'asc'
-      'コメントが少ない順'
+    elsif request.query_parameters[:sort] == 'favorites_count' && request.query_parameters[:order] == 'asc'
+      'いいねが少ない順'
     elsif request.query_parameters[:sort] == 'comments_count' && request.query_parameters[:order] == 'desc'
       'コメントが多い順'
+    elsif request.query_parameters[:sort] == 'comments_count' && request.query_parameters[:order] == 'asc'
+      'コメントが少ない順'
     else
-      'デフォルト'
+      '並び替え'
     end
   end
 
   def selected_user_sort_order
-    if request.query_parameters[:sort] == 'recipes_count' && request.query_parameters[:order] == 'asc'
+    if request.query_parameters[:sort] == 'recipes_count' && request.query_parameters[:order] == 'desc'
+    '投稿が多い順'
+    elsif request.query_parameters[:sort] == 'recipes_count' && request.query_parameters[:order] == 'asc'
       '投稿が少ない順'
-    elsif request.query_parameters[:sort] == 'recipes_count' && request.query_parameters[:order] == 'desc'
-      '投稿が多い順'
-    elsif request.query_parameters[:sort] == 'followers_count' && request.query_parameters[:order] == 'asc'
-      'フォロワーが少ない順'
     elsif request.query_parameters[:sort] == 'followers_count' && request.query_parameters[:order] == 'desc'
       'フォロワーが多い順'
-    elsif request.query_parameters[:sort] == 'followings_count' && request.query_parameters[:order] == 'asc'
-      'フォロー中が少ない順'
+    elsif request.query_parameters[:sort] == 'followers_count' && request.query_parameters[:order] == 'asc'
+      'フォロワーが少ない順'
     elsif request.query_parameters[:sort] == 'followings_count' && request.query_parameters[:order] == 'desc'
       'フォロー中が多い順'
+    elsif request.query_parameters[:sort] == 'followings_count' && request.query_parameters[:order] == 'asc'
+      'フォロー中が少ない順'
     else
-      'デフォルト'
+      '並び替え'
     end
   end
 
