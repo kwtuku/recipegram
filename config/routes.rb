93408e0727a8fb4bc00f39bf46406edb373b08b0
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-19 12:37:41 +0900

    アカウント削除時にパスワードを要求するようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   devise_scope :user do
+    get 'users/confirm_destroy', to: 'users/registrations#confirm_destroy'
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
