32321cfdcb8bf60d9c3053eddbf396945f7b5cce
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-23 21:33:32 +0900

    comments#createと#destroyの全ての条件でリダイレクト先もレコード数の増減のテストもするようにした

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,66 +1,104 @@
 require 'rails_helper'
 
 RSpec.describe 'Comments', type: :request do
-  describe 'POST /recipes/:recipe_id/comments' do
+  describe '#create' do
     let(:alice) { create :user, :no_image }
     let(:alice_recipe) { create :recipe, :no_image, user: alice }
 
-    context 'not signed in' do
-      it 'redirect_to new_user_session_path' do
+    context 'when not signed in' do
+      it 'redirects to new_user_session_path' do
+        comment_params = attributes_for(:comment)
+        post recipe_comments_path(alice_recipe), params: { comment: comment_params }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'does not increase Comment count' do
         comment_params = attributes_for(:comment)
         expect {
           post recipe_comments_path(alice_recipe), params: { comment: comment_params }
         }.to change { Comment.count }.by(0)
-        expect(response).to have_http_status(302)
-        expect(response).to redirect_to new_user_session_path
       end
     end
 
-    context 'signed in' do
-      it 'create comment' do
+    context 'when signed in' do
+      it 'redirects to recipe_path(commented recipe)' do
+        sign_in alice
+        comment_params = attributes_for(:comment)
+        post recipe_comments_path(alice_recipe), params: { comment: comment_params }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to recipe_path(alice_recipe)
+      end
+
+      it 'increases Comment count' do
         sign_in alice
         comment_params = attributes_for(:comment)
         expect {
           post recipe_comments_path(alice_recipe), params: { comment: comment_params }
-        }.to change { alice.comments.count }.by(1)
+        }.to change { Comment.count }.by(1)
+        .and change { alice.comments.count }.by(1)
       end
     end
   end
 
-  describe 'DELETE /recipes/:recipe_id/comments/:id' do
+  describe '#destroy' do
     let(:alice) { create :user, :no_image }
     let(:bob) { create :user, :no_image }
     let(:alice_recipe) { create :recipe, :no_image, user: alice }
     let!(:alice_comment) { create :comment, user: alice, recipe: alice_recipe }
 
-    context 'not signed in' do
-      it 'redirect_to new_user_session_path' do
-        expect {
-          get recipe_path(alice_recipe)
-          delete recipe_comment_path(alice_comment)
-        }.to change { alice.comments.count }.by(0)
+    context 'when not signed in' do
+      it 'redirects to new_user_session_path' do
+        get recipe_path(alice_recipe)
+        delete recipe_comment_path(alice_comment)
         expect(response).to have_http_status(302)
         expect(response).to redirect_to new_user_session_path
       end
+
+      it 'does not decrease Comment count' do
+        expect {
+          get recipe_path(alice_recipe)
+          delete recipe_comment_path(alice_comment)
+        }.to change { Comment.count }.by(0)
+        .and change { alice.comments.count }.by(0)
+      end
     end
 
-    context 'signed in as wrong user' do
-      it 'can not destroy other user comment' do
+    context 'when signed in as wrong user' do
+      it 'redirects to recipe_path(commented recipe)' do
+        sign_in bob
+        get recipe_path(alice_recipe)
+        delete recipe_comment_path(alice_comment)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to recipe_path(alice_recipe)
+      end
+
+      it 'does not decrease Comment count' do
         sign_in bob
         expect {
           get recipe_path(alice_recipe)
           delete recipe_comment_path(alice_comment)
-        }.to change { alice.comments.count }.by(0)
+        }.to change { Comment.count }.by(0)
+        .and change { alice.comments.count }.by(0)
       end
     end
 
-    context 'signed in as correct user' do
-      it 'destroy comment' do
+    context 'when signed in as correct user' do
+      it 'redirects to recipe_path(commented recipe)' do
+        sign_in alice
+        get recipe_path(alice_recipe)
+        delete recipe_comment_path(alice_comment)
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to recipe_path(alice_recipe)
+      end
+
+      it 'does not decrease Comment count' do
         sign_in alice
         expect {
           get recipe_path(alice_recipe)
           delete recipe_comment_path(alice_comment)
-        }.to change { alice.comments.count }.by(-1)
+        }.to change { Comment.count }.by(-1)
+        .and change { alice.comments.count }.by(-1)
       end
     end
   end
