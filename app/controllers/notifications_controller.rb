4222eab6258cbf8871b63d018ba57fcf806d4cf0
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-09-03 19:03:30 +0900

    notifications#indexのクエリを改善した

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.all.page(params[:page]).per(10)
+    @notifications = current_user.notifications.includes(notifiable: :user).all.page(params[:page]).per(10)
     @notifications.where(read: false).each do |notification|
       notification.update(read: true)
     end
