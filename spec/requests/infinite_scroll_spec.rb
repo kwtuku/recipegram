cd8503dcca5261bdebf0c6fcce0d2c97a33fdfa9
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-09-04 14:17:18 +0900

    不要な変数とスペースを削除、リンクの数を確かめる正規表現を変更した

--- spec/requests/infinite_scroll_spec.rb
+++ spec/requests/infinite_scroll_spec.rb
@@ -13,25 +13,25 @@ RSpec.describe 'InfiniteScroll', type: :request do
 
     context 'when not signed in and 20 items are already displayed' do
       it 'returns a 200 response' do
-        get infinite_scroll_path, params: {displayed_item_count: '20',  path: '' }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '20', path: '' }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders 20 item links' do
-        get infinite_scroll_path, params: {displayed_item_count: '20',  path: '' }, xhr: true
-        expect(response.body.scan(/recipes\/\d+/).size).to eq 20
+        get infinite_scroll_path, params: {displayed_item_count: '20', path: '' }, xhr: true
+        expect(response.body.scan(/\/recipes\/\d+/).size).to eq 20
       end
     end
 
     context 'when not signed in and 40 items are already displayed' do
       it 'returns a 200 response' do
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: '' }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: '' }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders correct items links' do
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: '' }, xhr: true
-        expect(response.body.scan(/recipes\/\d+/).size).to eq 20
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: '' }, xhr: true
+        expect(response.body.scan(/\/recipes\/\d+/).size).to eq 20
       end
     end
 
@@ -43,20 +43,18 @@ RSpec.describe 'InfiniteScroll', type: :request do
       before do
         random_users = User.all.sample(40)
         random_users.each { |user| alice.relationships.create(follow_id: user.id) }
-        followings = alice.followings
-        not_followings = User.all - followings
         create_list(:recipe, rand(1..10), :no_image, user: alice, updated_at: rand(1..100).minutes.ago)
       end
 
       it 'returns a 200 response' do
         sign_in alice
-        get infinite_scroll_path, params: {displayed_item_count: '20',  path: '' }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '20', path: '' }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders correct item links' do
         sign_in alice
-        get infinite_scroll_path, params: {displayed_item_count: '20',  path: '' }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '20', path: '' }, xhr: true
         feed_items[0..19].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         feed_items[20..39].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         feed_items[40..59].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
@@ -68,20 +66,18 @@ RSpec.describe 'InfiniteScroll', type: :request do
       before do
         random_users = User.all.sample(40)
         random_users.each { |user| alice.relationships.create(follow_id: user.id) }
-        followings = alice.followings
-        not_followings = User.all - followings
         create_list(:recipe, rand(1..10), :no_image, user: alice, updated_at: rand(1..100).minutes.ago)
       end
 
       it 'returns a 200 response' do
         sign_in alice
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: '' }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: '' }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders correct items links' do
         sign_in alice
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: '' }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: '' }, xhr: true
         feed_items[0..39].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         feed_items[40..59].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         not_feed_items.pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
@@ -166,7 +162,7 @@ RSpec.describe 'InfiniteScroll', type: :request do
       end
 
       it 'renders correct item links' do
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: "users/#{alice.username}" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: "users/#{alice.username}" }, xhr: true
         posted_recipes[0..39].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         posted_recipes[40..79].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         posted_recipes[80..99].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
@@ -181,7 +177,7 @@ RSpec.describe 'InfiniteScroll', type: :request do
       end
 
       it 'renders correct item links' do
-        get infinite_scroll_path, params: {displayed_item_count: '80',  path: "users/#{alice.username}" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '80', path: "users/#{alice.username}" }, xhr: true
         posted_recipes[0..79].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         posted_recipes[80..99].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         not_posted_recipes.pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
@@ -201,7 +197,7 @@ RSpec.describe 'InfiniteScroll', type: :request do
 
     context 'when 40 items are already displayed' do
       it 'returns a 200 response' do
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: "users/#{alice.username}/comments" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: "users/#{alice.username}/comments" }, xhr: true
         expect(response).to have_http_status(200)
       end
 
@@ -216,12 +212,12 @@ RSpec.describe 'InfiniteScroll', type: :request do
 
     context 'when 80 items are already displayed' do
       it 'returns a 200 response' do
-        get infinite_scroll_path, params: {displayed_item_count: '80',  path: "users/#{alice.username}/comments" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '80', path: "users/#{alice.username}/comments" }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders correct items links' do
-        get infinite_scroll_path, params: {displayed_item_count: '80',  path: "users/#{alice.username}/comments" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '80', path: "users/#{alice.username}/comments" }, xhr: true
         commented_recipes[0..79].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         commented_recipes[80..99].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         not_commented_recipes.pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
@@ -241,12 +237,12 @@ RSpec.describe 'InfiniteScroll', type: :request do
 
     context 'when 40 items are already displayed' do
       it 'returns a 200 response' do
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: "users/#{alice.username}/favorites" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: "users/#{alice.username}/favorites" }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders correct item links' do
-        get infinite_scroll_path, params: {displayed_item_count: '40',  path: "users/#{alice.username}/favorites" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '40', path: "users/#{alice.username}/favorites" }, xhr: true
         favored_recipes[0..39].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         favored_recipes[40..79].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         favored_recipes[80..99].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
@@ -256,12 +252,12 @@ RSpec.describe 'InfiniteScroll', type: :request do
 
     context 'when 80 items are already displayed' do
       it 'returns a 200 response' do
-        get infinite_scroll_path, params: {displayed_item_count: '80',  path: "users/#{alice.username}/favorites" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '80', path: "users/#{alice.username}/favorites" }, xhr: true
         expect(response).to have_http_status(200)
       end
 
       it 'renders correct items links' do
-        get infinite_scroll_path, params: {displayed_item_count: '80',  path: "users/#{alice.username}/favorites" }, xhr: true
+        get infinite_scroll_path, params: {displayed_item_count: '80', path: "users/#{alice.username}/favorites" }, xhr: true
         favored_recipes[0..79].pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
         favored_recipes[80..99].pluck(:id).each { |recipe_id| expect(response.body).to include "/recipes/#{recipe_id}" }
         not_favored_recipes.pluck(:id).each { |recipe_id| expect(response.body).to_not include "/recipes/#{recipe_id}" }
