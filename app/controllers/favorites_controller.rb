0a135317c70dadecfe52bb257fc582d8738c5aff
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-01 18:38:02 +0900

    refactor: ifの条件内で変数を定義するのをやめた、不要なインスタンス変数をローカル変数に変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    if @favorite = current_user.favorites.create(recipe_id: params[:recipe_id])
-      Notification.create_favorite_notification(@favorite)
-    end
+    return unless current_user.favorites.create(recipe_id: @recipe.id)
+
+    favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
+    Notification.create_favorite_notification(favorite)
   end
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
-    @favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
-    @favorite.destroy
+    favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
+    favorite.destroy
   end
 end
