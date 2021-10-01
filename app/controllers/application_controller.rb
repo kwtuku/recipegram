2b06fcc1c13c1707396c21f46d68a1d4f3015431
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-01 15:31:39 +0900

    refactor: 新規登録後とログイン後のリダイレクト先がdeviseのデフォルトと同じroot_pathになっているので記述を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(_resource)
-    root_path
-  end
-
-  def after_sign_up_path_for(_resource)
-    root_path
-  end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
