cbc3bfbae13984667877ebea8bbe49f494d580ba
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-02 13:36:23 +0900

    style test: rubocop -AでStyle/ClassAndModuleChildrenを解消、rubocopを無効にするコメントを追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,26 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :authenticate_scope!, only: %i[edit update confirm_destroy destroy]
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    prepend_before_action :authenticate_scope!, only: %i[edit update confirm_destroy destroy] # rubocop:disable Rails/LexicallyScopedActionFilter
 
-  def confirm_destroy; end
+    def confirm_destroy; end
 
-  def destroy
-    result = resource.destroy_with_password(destroy_params[:current_password])
-    if result
-      Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-      set_flash_message :notice, :destroyed
-      yield resource if block_given?
-      respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
-    else
-      render 'confirm_destroy'
+    # rubocop:disable Metrics/AbcSize
+    def destroy
+      if resource.destroy_with_password(destroy_params[:current_password])
+        Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+        set_flash_message :notice, :destroyed
+        yield resource if block_given?
+        respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
+      else
+        render 'confirm_destroy'
+      end
     end
-  end
+    # rubocop:enable Metrics/AbcSize
 
-  private
+    private
 
-  def destroy_params
-    params.require(:user).permit(:current_password)
+    def destroy_params
+      params.require(:user).permit(:current_password)
+    end
   end
 end
