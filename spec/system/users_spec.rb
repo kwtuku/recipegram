a2724243716dba088414d13e61c093121ca37b28
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-05 14:31:08 +0900

    style: 事前にレコードを作成する記述をlet!からbeforeに移動、レコード数を検証する記述の変更、定数を変数に変更、current_pathの確認を削除

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  describe 'sign up' do
+  let(:ellen) { create :user, :no_image }
+
+  describe 'signs up' do
     let(:alice) { build :user, :no_image }
 
     it 'signs up', js: true do
       visit root_path
       click_link '登録'
-      expect(current_path).to eq new_user_registration_path
       expect(page).to have_button 'signup', disabled: true
       fill_in 'user[username]', with: alice.username
       fill_in 'user[nickname]', with: alice.nickname
       fill_in 'user[email]', with: alice.email
       fill_in 'user[password]', with: alice.password
       fill_in 'user[password_confirmation]', with: alice.password_confirmation
-      expect{ click_button 'signup' }.to change { User.count }.by(1)
+      expect { click_button 'signup' }.to change(User, :count).by(1)
       expect(page).to have_content 'アカウント登録が完了しました。'
     end
 
@@ -24,11 +25,11 @@ RSpec.describe 'Users', type: :system do
       expect(page).to have_field 'user[username]', with: ''
       click_link href: generate_username_path
       expect(page).to have_selector '.rspec_has_generated_username'
-      VALID_USERNAME_REGEX = /\A[a-zA-Z0-9[-][_]]+\z/
-      expect(page).to have_field 'user[username]', with: VALID_USERNAME_REGEX
-      GENERATED_USERNAME = find_field('user[username]').value
-      expect(GENERATED_USERNAME).to_not eq ''
-      expect(User.exists?(username: GENERATED_USERNAME)).to eq false
+      valid_username_regex = /\A[a-zA-Z0-9_-]+\z/
+      expect(page).to have_field 'user[username]', with: valid_username_regex
+      generated_username = find_field('user[username]').value
+      expect(generated_username).not_to eq ''
+      expect(User.exists?(username: generated_username)).to eq false
     end
   end
 
@@ -38,7 +39,6 @@ RSpec.describe 'Users', type: :system do
     it 'signs in', js: true do
       visit root_path
       click_link 'ログイン'
-      expect(current_path).to eq new_user_session_path
       expect(page).to have_button 'signin', disabled: true
       fill_in 'user[email]', with: alice.email
       fill_in 'user[password]', with: alice.password
@@ -60,7 +60,6 @@ RSpec.describe 'Users', type: :system do
       sign_in bob
       find('.rspec_header_dropdown_trigger').click
       click_link 'アカウント編集'
-      expect(current_path).to eq edit_user_registration_path
       expect(page).to have_button 'update_account', disabled: true
       fill_in 'user[username]', with: 'after'
       fill_in 'user[current_password]', with: bob.password
@@ -73,7 +72,6 @@ RSpec.describe 'Users', type: :system do
       sign_in bob
       find('.rspec_header_dropdown_trigger').click
       click_link 'アカウント編集'
-      expect(current_path).to eq edit_user_registration_path
       expect(page).to have_button 'update_account', disabled: true
       fill_in 'user[email]', with: 'after@example.com'
       fill_in 'user[current_password]', with: bob.password
@@ -85,11 +83,13 @@ RSpec.describe 'Users', type: :system do
     describe 'updates password' do
       let(:bob) { create :user, :no_image, password: 'old_password', password_confirmation: 'old_password' }
 
-      it 'signs in with new password after a password updated', js: true do
+      before do
         sign_in bob
         find('.rspec_header_dropdown_trigger').click
         click_link 'アカウント編集'
-        expect(current_path).to eq edit_user_registration_path
+      end
+
+      it 'signs in with new password after a password updated', js: true do
         expect(page).to have_button 'update_account', disabled: true
         fill_in 'user[password]', with: 'new_password'
         fill_in 'user[password_confirmation]', with: 'new_password'
@@ -97,7 +97,6 @@ RSpec.describe 'Users', type: :system do
         click_button 'update_account'
         expect(page).to have_content 'アカウント情報を変更しました。'
         sign_out
-
         visit root_path
         click_link 'ログイン'
         fill_in 'user[email]', with: bob.email
@@ -107,10 +106,6 @@ RSpec.describe 'Users', type: :system do
       end
 
       it 'does not sign in with old password after a password updated', js: true do
-        sign_in bob
-        find('.rspec_header_dropdown_trigger').click
-        click_link 'アカウント編集'
-        expect(current_path).to eq edit_user_registration_path
         expect(page).to have_button 'update_account', disabled: true
         fill_in 'user[password]', with: 'new_password'
         fill_in 'user[password_confirmation]', with: 'new_password'
@@ -118,7 +113,6 @@ RSpec.describe 'Users', type: :system do
         click_button 'update_account'
         expect(page).to have_content 'アカウント情報を変更しました。'
         sign_out
-
         visit root_path
         click_link 'ログイン'
         fill_in 'user[email]', with: bob.email
@@ -130,13 +124,13 @@ RSpec.describe 'Users', type: :system do
   end
 
   describe 'updates user' do
-    let(:carol) { create :user, :no_image, nickname: 'carol', profile: "I'm carol." }
+    let(:carol) { create :user, :no_image, nickname: 'carol', profile: 'I am carol.' }
+    let(:dave) { create :user }
 
     it 'updates nickname', js: true do
       sign_in carol
       find('.rspec_header_dropdown_trigger').click
       click_link 'マイページ'
-      expect(current_path).to eq user_path(carol)
       click_link 'プロフィールを編集'
       expect(page).to have_button 'update_user', disabled: true
       fill_in 'user[nickname]', with: 'キャロル'
@@ -149,7 +143,6 @@ RSpec.describe 'Users', type: :system do
       sign_in carol
       find('.rspec_header_dropdown_trigger').click
       click_link 'マイページ'
-      expect(current_path).to eq user_path(carol)
       click_link 'プロフィールを編集'
       expect(page).to have_button 'update_user', disabled: true
       fill_in 'user[profile]', with: 'キャロルです。'
@@ -158,36 +151,30 @@ RSpec.describe 'Users', type: :system do
       expect(page).to have_content 'プロフィールを変更しました。'
     end
 
-    let(:dave) { create :user, :no_image }
-
     it 'updates user_image', js: true do
       sign_in dave
       before_image_url = dave.user_image.url
       find('.rspec_header_dropdown_trigger').click
       click_link 'マイページ'
-      expect(current_path).to eq user_path(dave)
       click_link 'プロフィールを編集'
       expect(page).to have_button 'update_user', disabled: true
-      attach_file 'user[user_image]', "#{Rails.root}/spec/fixtures/user_image_sample_after.jpg", visible: false
+      attach_file 'user[user_image]', Rails.root.join('spec/fixtures/user_image_sample_after.jpg'), visible: false
       click_button 'update_user'
       after_image_url = dave.reload.user_image.url
-      expect(before_image_url).to_not eq after_image_url
+      expect(before_image_url).not_to eq after_image_url
       expect(page).to have_content 'プロフィールを変更しました。'
     end
   end
-
-  let(:ellen) { create :user, :no_image }
 
   it 'destroys account', js: true do
     sign_in ellen
     find('.rspec_header_dropdown_trigger').click
     click_link 'アカウント削除手続き'
-    expect(current_path).to eq users_confirm_destroy_path
     expect(page).to have_button 'destroy_account', disabled: true
     fill_in 'user[current_password]', with: ellen.password
-    expect{
+    expect  do
       click_button 'destroy_account'
-    }.to change { User.count }.by(-1)
+    end.to change(User, :count).by(-1)
     expect(page).to have_content 'アカウントを削除しました。またのご利用をお待ちしております。'
   end
 end
