2832e87d2a56d9b79e4fa307460ef9ec96039184
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-06 19:55:00 +0900

    test: 不要なsequenceを削除、ユーザーのニックネームとプロフィールの長さに関するバリデーションのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,8 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:nickname) }
+    it { is_expected.to validate_length_of(:nickname).is_at_most(30) }
+    it { is_expected.to validate_length_of(:profile).is_at_most(500) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
