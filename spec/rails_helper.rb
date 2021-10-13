c1ac30ce44804a120082fa4f595825c05d81d0c3
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-13 21:30:19 +0900

    chore: rspec実行時にcarrierwaveによる画像を削除するように設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.after(:all) do
+    FileUtils.rm_rf(Dir[Rails.root.join('public/uploads')]) if Rails.env.test?
+  end
+
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include FactoryBot::Syntax::Methods
   config.include SignInModule, type: :system
