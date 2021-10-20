2b7c90adb8400ded37bb40fef9f018e0b8e47b0b
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-20 20:25:22 +0900

    refactor: @resultsの中身をviewで必要なものだけに修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,34 +19,30 @@ class HomeController < ApplicationController
 
     sort = { '0' => { name: params[:sort], dir: params[:order] } }
 
-    recipe_title_results = Recipe.ransack({ title_has_every_term: @q_value, s: sort }).result
-      .eager_load(:favorites, :comments, :tags, :tag_taggings)
-    recipe_body_results = Recipe.ransack({ body_has_every_term: @q_value, s: sort }).result
-      .eager_load(:favorites, :comments, :tags, :tag_taggings)
-    user_nickname_results = User.ransack({ nickname_has_every_term: @q_value, s: sort }).result.eager_load(:recipes)
-    user_profile_results = User.ransack({ profile_has_every_term: @q_value, s: sort }).result.eager_load(:recipes)
-    tag_name_results = Tag.ransack({ name_has_every_term: @q_value, taggings_count_gteq: '1', s: sort }).result
-
-    @results = {
-      recipe_title: recipe_title_results.page(params[:page]).per(10),
-      recipe_body: recipe_body_results.page(params[:page]).per(10),
-      user_nickname: user_nickname_results.page(params[:page]).per(10),
-      user_profile: user_profile_results.page(params[:page]).per(10),
-      tag_name: tag_name_results.page(params[:page]).per(10)
+    results = {
+      recipe_title: Recipe.ransack({ title_has_every_term: @q_value, s: sort }).result
+        .eager_load(:favorites, :comments, :tags, :tag_taggings),
+      recipe_body: Recipe.ransack({ body_has_every_term: @q_value, s: sort }).result
+        .eager_load(:favorites, :comments, :tags, :tag_taggings),
+      user_nickname: User.ransack({ nickname_has_every_term: @q_value, s: sort }).result.eager_load(:recipes),
+      user_profile: User.ransack({ profile_has_every_term: @q_value, s: sort }).result.eager_load(:recipes),
+      tag_name: Tag.ransack({ name_has_every_term: @q_value, taggings_count_gteq: '1', s: sort }).result
     }
 
+    @results = results[@source.to_sym].page(params[:page]).per(10)
+
     @result_counts = {
-      title: recipe_title_results.size,
-      body: recipe_body_results.size,
-      nickname: user_nickname_results.size,
-      profile: user_profile_results.size,
-      name: tag_name_results.size
+      title: results[:recipe_title].size,
+      body: results[:recipe_body].size,
+      nickname: results[:user_nickname].size,
+      profile: results[:user_profile].size,
+      name: results[:tag_name].size
     }
 
     return unless @source == 'tag_name'
 
     @tagged_recipes = {}
-    @results[:tag_name].each do |tag|
+    @results.each do |tag|
       @tagged_recipes.store(tag.name, Recipe.tagged_with(tag.name).first(6))
     end
   end
