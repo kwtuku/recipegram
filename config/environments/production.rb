348e8b808c8c1b28713346be71c601a164317e13
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-10-23 20:40:07 +0900

    feat: redisを導入してトップページのおすすめのレシピとユーザーをキャッシュ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'], expires_in: 1.day }
 end
