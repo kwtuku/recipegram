d33af6564b640228ca193bdd39c25c5bd7983ca9
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-11-15 21:28:43 +0900

    fix: ransackのコードを参考にして、カスタム述語でPostgreSQLのワイルドカードをエスケープする処理を追加

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,7 +1,7 @@
 Ransack.configure do |config|
   config.add_predicate 'has_every_term', arel_predicate: 'matches_all', formatter: proc { |v|
     v.split(/[[:blank:]]+/).map do |t|
-      "%#{t}%"
+      "%#{t.to_s.gsub(/([\\%_.])/, '\\\\\\1')}%"
     end
   }
 end
