0a527528a1e7c05c37cace61c0068976f9eb4fcc
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-11-16 19:44:47 +0900

    refactor: recipeもキャッシュするようにしてクエリを改善

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.includes(notifiable: :user).all.page(params[:page]).per(10)
+    @notifications = current_user.notifications.preload(notifiable: %i[recipe user]).page(params[:page]).per(10)
     @notifications.where(read: false).each do |notification|
       notification.update(read: true)
     end
