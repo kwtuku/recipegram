c286f230b45416850dff50f83ea929641d3d1758
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-01-21 13:58:50 +0900

    chore: 既にコードがあるのでeslintのエラーをすべてオフに変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,27 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
   rules: {
+    'consistent-return': 'off',
+    'func-names': 'off',
+    'import/first': 'off',
+    'import/newline-after-import': 'off',
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-globals': 'off',
+    'no-undef': 'off',
+    'no-unused-expressions': 'off',
+    'no-use-before-define': 'off',
+    'object-shorthand': 'off',
+    'prefer-arrow-callback': 'off',
+    'prefer-const': 'off',
+    'prefer-destructuring': 'off',
+    'prefer-template': 'off',
+    radix: 'off',
   },
 };
