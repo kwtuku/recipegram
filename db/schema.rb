a2fad2b4addf963ee87eab0d05b3b9c0f6d682c0
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-01-27 17:32:34 +0900

    refactor: recipeテーブルにuserテーブルの外部キー制約とインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_235527) do
+ActiveRecord::Schema.define(version: 2022_01_27_082514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_10_09_235527) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
     t.string "recipe_image"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
@@ -109,6 +110,7 @@ ActiveRecord::Schema.define(version: 2021_10_09_235527) do
 
   add_foreign_key "comments", "recipes"
   add_foreign_key "comments", "users"
+  add_foreign_key "recipes", "users"
   add_foreign_key "relationships", "users"
   add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "taggings", "tags"
