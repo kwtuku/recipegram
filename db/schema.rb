88ac3b3f4d94c0acf8269a853bd37db59ac84b03
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-02-02 23:54:19 +0900

    perf: ユーザーのフォロワーとフォロー中の数をcounter_cultureでキャッシュ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_120437) do
+ActiveRecord::Schema.define(version: 2022_02_02_135425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,8 @@ ActiveRecord::Schema.define(version: 2022_02_02_120437) do
     t.string "user_image"
     t.string "username"
     t.integer "recipes_count", default: 0, null: false
+    t.integer "followers_count", default: 0, null: false
+    t.integer "followings_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
