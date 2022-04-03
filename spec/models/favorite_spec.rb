59444c7009b46d36ce8d24b45c67b47130f5971b
kwtuku <76758720+kwtuku@users.noreply.github.com>
2022-04-03 21:34:09 +0900

    style: subjectを使わないように変更

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  subject { described_class.new(recipe_id: create(:recipe, :no_image).id, user_id: create(:user, :no_image).id) }
-
-  it { is_expected.to validate_uniqueness_of(:recipe_id).scoped_to(:user_id) }
+  it do
+    favorite = described_class.new(recipe_id: create(:recipe).id, user_id: create(:user).id)
+    expect(favorite).to validate_uniqueness_of(:recipe_id).scoped_to(:user_id)
+  end
 end
